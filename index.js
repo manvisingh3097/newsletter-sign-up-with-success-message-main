06bbc22671f7e5a6aeaa159c1469f0c27ee317c1
manvisingh3097 <singh.thakurmanvi@gmail.com>
2024-03-17 21:55:24 +0530

    js

---
+++ index.js
@@ -0,0 +1,13 @@
+let email = document.getElementById('email')
+
+function validform () {
+
+    let estvalid = true;
+    const emailInput = document.getElementById( 'email' );
+
+    if(emailInput.value === "True"){
+        alert("Wow ye to crazy hogya")
+    }
+    else("arey bharna rey")
+
+}
